ef23d7db05bd2b597cda86c474698ffb6e11d3db
ahseneremmouche <ahsene.remmouche@outlook.com>
2020-12-22 22:29:40 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-//Unsplash API
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+const downloadLink = document.getElementById('download-link');
+
+let photosArray = [];
+
+//Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM and
+function displayPhotos() {
+  //Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    //create Download link
+    const link = document.createElement('a');
+    setAttributes(link, {
+      href: photo.links.download,
+    });
+
+    link.innerHTML = 'Click to download';
+    imageContainer.appendChild(link);
+
+    const item = document.createElement('a');
+    //item.setAttribute('href', photo.links.html);
+    //item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    //img.setAttribute('src', photo.urls.regular);
+    //img.setAttribute('alt', photo.alt_description);
+    //img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Unsplash API
 const count = 10;
 const apiKey = '25jIjLz3JJbWckjCdB0hhzhMI4pxlaGe5rJObuBMdcE';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
@@ -6,12 +57,13 @@ const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photosArray = await response.json();
+    displayPhotos();
+    console.log(photosArray);
   } catch (err) {
     console.log('Ooops no photos', err);
   }
 }
 
-//On Load
+// On Load
 getPhotos();
