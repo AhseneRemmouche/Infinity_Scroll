1452dc199a1b81b9c85b1ef227c8327f3ef092cb
ahseneremmouche <ahsene.remmouche@outlook.com>
2020-12-23 19:43:01 +0100

    third commit

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 const downloadLink = document.getElementById('download-link');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+
+//Check if all images werw loaded
+function imageLoaded() {
+  console.log('image loaded');
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log('ready = ', ready);
+  }
+}
 
 //Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -13,6 +27,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements For Links & Photos, Add to DOM and
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log('total images ', totalImages);
   //Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -43,6 +60,9 @@ function displayPhotos() {
       title: photo.alt_description,
     });
 
+    //Event Listeners, chexk when each photo is finished loading
+    img.addEventListener('load', imageLoaded);
+
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -64,6 +84,29 @@ async function getPhotos() {
     console.log('Ooops no photos', err);
   }
 }
+
+//Check to see if scrolling near bottom of page, Load more photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    console.log('window.innerHeight', window.innerHeight);
+    console.log('window.scrollY', window.scrollY);
+    console.log(
+      'window.innerHeight + scrollY',
+      window.scrollY + window.innerHeight
+    );
+    console.log(
+      'document.body.offsetHeight - 1000',
+      document.body.offsetHeight - 1000
+    );
+    ready = false;
+    getPhotos();
+    console.log('load more');
+    console.log('ready ', ready);
+  }
+});
 
 // On Load
 getPhotos();
